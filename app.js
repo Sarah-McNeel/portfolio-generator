ba3dc4b5d46b21835e892217f34099c30badaa24
SDM80 <sdawn8912@gmail.com>
2020-11-19 12:17:02 -0600

    created html

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
+const [name, github] = profileDataArgs;
 
-
-const printProfileData = profileDataArr => {
-    //this
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    //is the same as this...
-    profileDataArr.foreach(profileItem => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio Complete! Checkout index.html to see the output!');
+});
